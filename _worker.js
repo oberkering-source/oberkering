f8bf507d9b1fa7525b71ec939c94d8cd6f586230
oberkering-source <oberkering@gmail.com>
2025-09-04 23:59:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from "cloudflare:sockets";
 // Variables
 const rootDomain = "oberkering.workers.dev"; // Ganti dengan domain utama kalian
 const serviceName = "ober"; // Ganti dengan nama workers kalian
-const apiKey = "ab32a8ef5ddc8ca3980e64a2ec04c9ea6017c"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
+const apiKey = "dOeZ-rwB-u_awvsnqN6HHJV97tKTRWkDoLFy8CMb"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = "oberkering@gmail.com"; // Ganti dengan email yang kalian gunakan
 const accountID = "cbaa4c6721e9e740ba488da2d74d977a"; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
 const zoneID = ""; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
