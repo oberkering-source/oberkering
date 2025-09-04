176cb8d0412108e160ff8a6711f9361e5727b44e
oberkering-source <oberkering@gmail.com>
2025-09-05 00:33:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,12 +5,12 @@ import { connect } from "cloudflare:sockets";
 // Variables
 const rootDomain = "oberkering.workers.dev"; // Ganti dengan domain utama kalian
 const serviceName = "ober"; // Ganti dengan nama workers kalian
-const apiKey = "ab32a8ef5ddc8ca3980e64a2ec04c9ea6017c"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
+const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = "oberkering@gmail.com"; // Ganti dengan email yang kalian gunakan
 const accountID = "cbaa4c6721e9e740ba488da2d74d977a"; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
 const zoneID = ""; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
 let isApiReady = false;
-let proxyIP = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/kvProxyList.json";
+let proxyIP = "";
 let cachedProxyList = [];
 
 // Constant
